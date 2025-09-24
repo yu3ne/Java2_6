07b2a96e87a2227eb0b05cb8ee9263377de4a343
yu3ne <bsi25029@uhe.ac.jp>
2025-09-24 14:06:06 +0900

    init

---
+++ src/Exp6_2.java
@@ -0,0 +1,19 @@
+
+public class Exp6_2 {
+
+	public static void main(String[] args) {
+
+		int n = 13;
+		int step = 0;
+
+		while (n != 1) {
+			step++;
+			if (n % 2 == 0) {
+				n = n / 2;
+			} else {
+				n = 3 * n + 1;
+			}
+		}
+	}
+
+}
